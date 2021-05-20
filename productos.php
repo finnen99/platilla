60779d446b74f9517deed8b55d33d69dcfe19985
Marcos <kito990831@gmail.com>
2021-05-20 13:34:51 -0500

    Subida de archivos

---
+++ productos.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>IDM</title>
+</head>
+
+<body>
+    <header class="landing-page">
+        <div class="cover"></div>
+        <nav class="navigation">
+            <a href="#productos">Productos</a>
+            <a href="#empresas">Empresas</a>
+            <a href="#nosotros">Nosotros</a>
+        </nav>
+        <div class="presentation">
+            <img src="https://logodownload.org/wp-content/uploads/2018/07/prime-video-1.png" alt="Logo">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, ducimus.</p>
+            <a href="#productos">Ver Productos</a>
+        </div>
+    </header>
+
+    <section id="productos" class="products d-flex">
+        <h2 class="title">Productos</h2>
+        <div class="container">
+            <!-- Card Product -->
+            <div class="card">
+                <div class="imgBox">
+                    <img src="https://www.pequerecetas.com/wp-content/uploads/2020/10/tacos-mexicanos.jpg" alt="Producto">
+                </div>
+                <div class="info d-flex">
+                    <h3 class="Name-Product">Lorem, ipsum dolor.</h3>
+                    <p class="Price">$25</p>
+                    <a href="#">Detalles</a>
+                </div>
+            </div>
+            <!-- Cierre del Card Product -->
+        </div>
+    </section>
+
+    <section id="empresas" class="companies d-flex">
+        <h2 class="title">Empresas</h2>
+    <div class="container">
+        <div class="box-company">
+            <a href="#"><img src="https://logos-marcas.com/wp-content/uploads/2020/11/Uber-Eats-Logo.png" alt="logo"></a>
+        </div>
+    </div>
+    </section>
+
+    <section id="nosotros">
+        <h2 class="title">Nosotros</h3>
+        <div class="container">
+            <div class="card-person">
+                <div class="img-box-person">
+                    <img src="https://persona-app-es.herokuapp.com/assets/personas/2-03cad6d57866b41e354c240f94fbe4a930d54ffe73e5e864ce28822c9d170c1e.png" alt="Persona">
+                </div>
+                <div class="info-person">
+                    <h3>Peter Henderson</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore enim optio dolores magnam, neque id?</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, placeat.</p>
+    </footer>
+</body>
+
+</html>
